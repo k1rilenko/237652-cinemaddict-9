25c7c0dd420cea62d6c64b3dfdc5ebdf44f2c5c8
Roman Kirilenko <cap.spouk@gmail.com>
2019-10-03 22:56:04 +0300

    fix

--- src/components/data.js
+++ src/components/data.js
@@ -20,7 +20,8 @@ const dataValue = {
   duration: getrandomInteger(49, 140),
   genre: [`Crime`, `Fantasy`, `Historical`, `Horror`, `Mystery`],
   rating: getrandomInteger(1, 9),
-  comments: getrandomInteger(1, 40)
+  comments: getrandomInteger(1, 40),
+  comment1: getrandomInteger(1, 40)
 };
 const generaTeFilmData = (data) => {
   return {
